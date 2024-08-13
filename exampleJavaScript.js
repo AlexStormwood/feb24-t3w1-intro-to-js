d1254a6656df8e1a14648920c8060dd2ae1a6c08
Alex <alex.holder@bigfootds.com>
2024-08-13 20:58:53 +1000

    feat: log bananas

---
+++ exampleJavaScript.js
@@ -0,0 +1,29 @@
+console.log("Hello world!");
+
+
+
+
+
+
+
+let someMessage = "Bananas!";
+
+
+
+console.log(someMessage);
+
+
+someMessage = 1000;
+
+
+
+
+console.log(someMessage);
+
+
+
+
+
+
+
+console.log("Another hello world!");
